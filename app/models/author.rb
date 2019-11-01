c8859246bc3e6a74781e344bf2ff720559744e61
Wanda <wanda.baltzer@student.hpi.de>
2019-11-01 20:51:03 +0100

    an instance of Author can now be created with first name, last name, and homepage; updated test for #3

---
+++ app/models/author.rb
@@ -0,0 +1,9 @@
+class Author < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :homepage, presence: true
+
+  def name
+    [first_name, last_name].join(' ')
+  end
+end

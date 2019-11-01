a9a9180c1615409dfd54f4e4dbba8b947e47eef5
Wanda <wanda.baltzer@student.hpi.de>
2019-11-01 17:04:33 +0100

    changed /author/new to /authors/new

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
-  resources :author
+  resources :authors
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

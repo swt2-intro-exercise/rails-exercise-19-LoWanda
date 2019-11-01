f2ab23b9089bc9940172aa8bc386fb5054f9314f
Wanda <wanda.baltzer@student.hpi.de>
2019-11-01 16:33:12 +0100

    added a new author page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
+  resources :author
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

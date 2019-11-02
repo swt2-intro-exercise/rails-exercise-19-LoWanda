f3940274952064f6b6368665a7885a941493b1b2
Wanda <wanda.baltzer@student.hpi.de>
2019-11-02 12:15:32 +0100

    Failing test for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,11 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should display 'error' when a user tries to create an invalid object" do
+    visit new_author_path
+    fill_in('author[first_name]', with: 'Alan')
+    fill_in('author[last_name]', with: 'Turing')
+    expect(page).to have_text 'error'
+  end
 end

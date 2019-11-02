69ade247710ed5332fe7a158021563a1ebe6d37a
Wanda <wanda.baltzer@student.hpi.de>
2019-11-02 15:54:13 +0100

    added edit page, test not working though (#6)

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe "Author edit page", type: :feature do
   it "should render without error" do
-    visit edit_author_path
+    visit authors_path
+    expect(page).to have_link('Edit')
+    save_and_open_page
   end
 end

31b43733d1c882f0a856e5c71180b4b9bf688b57
Wanda <wanda.baltzer@student.hpi.de>
2019-11-02 16:33:04 +0100

    added a test to check whether editing an author works correctly for #6

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -8,4 +8,16 @@ describe "Author edit page", type: :feature do
 	    homepage: 'http://wikipedia.org/Alan_Turing')
     visit 'authors/1/edit'
   end
+
+  it "should update the author after editing" do 
+  	author = Author.create(
+	    first_name: 'Alan', 
+	    last_name: 'Turing', 
+	    homepage: 'http://wikipedia.org/Alan_Turing')
+    visit 'authors/1/edit'
+    fill_in('author[first_name]', with: 'Lara')
+    click_button('Update Author')
+    author.reload
+    expect(author.name).to eq('Lara Turing')
+  end
 end

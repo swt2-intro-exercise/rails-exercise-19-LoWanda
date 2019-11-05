b92a6f214f6759fe7dd6fdd229c7c6e6cbd70294
Wanda <wanda.baltzer@student.hpi.de>
2019-11-05 16:24:07 +0100

    updated failing test for #11

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -4,7 +4,11 @@ describe "Show Paper page", type: :feature do
   it "should display the author's full name" do
     paper = create(:paper)
     author = create(:author)
+    paper.authors << Author.new(
+    	first_name: author.first_name, 
+    	last_name: author.last_name,
+    	homepage: author.homepage)
     visit paper_path(paper)
-    expect(page).to have_text(author.name)
+    expect(page).to have_text(paper.authors.first.name)
   end
 end

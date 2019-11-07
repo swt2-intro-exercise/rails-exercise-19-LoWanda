840d50564dc95d10a961a04b5e75220b4b7343b7
Wanda <wanda.baltzer@student.hpi.de>
2019-11-07 10:35:52 +0100

    paper show page shows the author's full name, updated test for #11

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -2,13 +2,9 @@ require 'rails_helper'
 
 describe "Show Paper page", type: :feature do 
   it "should display the author's full name" do
-    paper = create(:paper)
     author = create(:author)
-    paper.authors << Author.new(
-    	first_name: author.first_name, 
-    	last_name: author.last_name,
-    	homepage: author.homepage)
+    paper = create(:paper)
     visit paper_path(paper)
-    expect(page).to have_text(paper.authors.first.name)
+    expect(page).to have_text(paper.authors.name)
   end
 end

198f2d2224e81eb878e79f19f9444a9172e0e5ca
Wanda <wanda.baltzer@student.hpi.de>
2019-11-07 12:26:15 +0100

    paper index page filters papers by year, #14

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ApplicationRecord
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
+  scope :published, -> (time) { where("year = ?", time) if time.present? }
 end

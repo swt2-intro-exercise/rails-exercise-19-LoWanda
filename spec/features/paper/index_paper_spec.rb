a321bbe265a7844355a2cf0ac10e3f6943e5ee1d
Wanda <wanda.baltzer@student.hpi.de>
2019-11-07 12:14:32 +0100

    Failing test for #14

---
+++ spec/features/paper/index_paper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Index Paper Page", type: :feature do
+  it "should allow filtering papers by year" do
+  	paper1 = Paper.create(
+  		title: 'title',
+  		venue: 'venue',
+  		year: '1950')
+  	paper2 = Paper.create(
+  		title: 'title',
+  		venue: 'venue',
+  		year: '1968')
+  	visit '/papers?year=1950'
+  	expect(page).to have_text('1950')
+  	expect(page).to have_no_text('1968')
+  end
+end

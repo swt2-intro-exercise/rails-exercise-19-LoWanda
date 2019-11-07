591b05d0da691a4aaa5c7866010235da090fd37e
Wanda <wanda.baltzer@student.hpi.de>
2019-11-07 11:31:26 +0100

    Failing test for #13

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 describe "Edit Paper page", type: :feature do 
+  
+  before :each do
+    @paper = create(:paper)   
+  end
+
   it "should have a single multiple select box" do
-    paper = create(:paper)
-    visit edit_paper_path(paper)
+    visit edit_paper_path(@paper)
     expect(page).to have_css 'select[multiple]'
+  end
+
+  it "should update the paper after adding another author" do 
+  	@author = Author.create(
+  		first_name: 'Peter',
+  		last_name: 'Plagiarist',
+  		homepage: 'homepage')
+  	visit edit_paper_path(@paper)
+    select @author.name, from: 'paper[author_ids][]'
+    save_and_open_page
+    click_button 'Update Paper'
+    expect(@paper.authors).to include('Peter Plagiarist', 'Alan Turing')
   end
 end
